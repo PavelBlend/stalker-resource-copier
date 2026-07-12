30b4bf962afa5507455809880160f70d5313f763
PavelBlend <stalkermodkytia@yandex.ru>
2026-07-12 08:23:37 +0300

    refactor

--- make_release.py
+++ make_release.py
@@ -4,15 +4,18 @@ import stalker_resource_copier
 
 
 prog_name = 'stalker resource copier'
-readme_file = 'README.md'
+readme_file = 'README'
 xray_dir = 'xray'
 py_ext = '.py'
 pyw_ext = '.pyw'
+md_ext = '.md'
 
 source_name = prog_name.replace(' ', '_')
 release_name = prog_name.replace(' ', '-')
 ver_str = ('.'.join(map(str, stalker_resource_copier.VERSION)))
 file_name = '{0}-{1}.zip'.format(release_name, ver_str)
+
+compss_type = zipfile.ZIP_DEFLATED
 
 # write .zip file
 with zipfile.ZipFile(file_name, 'w') as release_zip:
@@ -20,20 +23,20 @@ with zipfile.ZipFile(file_name, 'w') as release_zip:
     # write main source file
     main_src = source_name + py_ext
     main_dist = '{0}/{0}{1}'.format(source_name, pyw_ext)
-    release_zip.write(main_src, main_dist, compress_type=zipfile.ZIP_DEFLATED)
+    release_zip.write(main_src, main_dist, compress_type=compss_type)
 
-    release_zip.write(
-        readme_file,
-        '{0}/{1}'.format(source_name, readme_file),
-        compress_type=zipfile.ZIP_DEFLATED
-    )
+    # write readme file
+    readme_src = readme_file + md_ext
+    readme_dist = '{0}/{1}{2}'.format(source_name, readme_file, md_ext)
+    release_zip.write(readme_src, readme_dist, compress_type=compss_type)
+
     for file in os.listdir(xray_dir):
         name, ext = os.path.splitext(file)
         if ext == py_ext:
             release_zip.write(
                 '{0}\\{1}{2}'.format(xray_dir, name, py_ext),
                 '{0}/{1}/{2}.py'.format(source_name, xray_dir, name),
-                compress_type=zipfile.ZIP_DEFLATED
+                compress_type=compss_type
             )
 
 input('Finish!')
